4b8af137a0def88e054432191dd523be20e8a2f0
Karri Surya Venkata Prakash Rao <karriprakash342@gmail.com>
2022-06-21 08:01:53 +0530

    Second Commit for rest application

--- src/main/java/com/spring/project/util/EmployeeUtil.java
+++ src/main/java/com/spring/project/util/EmployeeUtil.java
@@ -5,9 +5,9 @@ import com.spring.project.model.Employee;
 public interface EmployeeUtil {
 
 	public static void updateHraAndTa(Employee employee) {
-		var salary = employee.getEmpSal();
-		var hra = salary * 20/100.0;
-		var ta = salary * 12/100.0;
+		double salary = employee.getEmpSal();
+		double hra = salary * 20/100.0;
+		double ta = salary * 12/100.0;
 		employee.setEmpHra(hra);
 		employee.setEmpTa(ta);
 	}
